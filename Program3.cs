97522687941ca6b511ef2ad1a3db5036fcc60cf0
CQta <awerin134@gmail.com>
2022-12-05 23:08:47 +0300

    3

--- Program3.cs
+++ Program3.cs
@@ -1,16 +1,49 @@
-﻿void Get_Dot(double k1, double k2, double b1, double b2)
+﻿int[,] GetArray(int m, int n) 
 {
-    Console.WriteLine("X = " + (b2-b1)/(k1-k2));
-    Console.WriteLine("Y = " + (k2*((b2-b1)/(k1-k2))+b2));    
+    int[,] result = new int[m, n];
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(0, 9);
+        }
+    }
+
+    return result;
 }
 
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]}\t ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+void Find_average(int[,] inArray)
+{
+    double sum;
+    for (int i = 0; i < inArray.GetLength(1); i++)
+    {
+        sum = 0;
+        for (int j = 0; j < inArray.GetLength(0); j++)
+        {
+            sum += inArray[j,i];
+        }
+        Console.Write(Math.Round(sum/inArray.GetLength(0),2) + " ");
+    }
+}
 Console.Clear();
-Console.Write("Введите k1: ");
-double k1 = double.Parse(Console.ReadLine()!);
-Console.Write("Введите k2: ");
-double k2 = double.Parse(Console.ReadLine()!);
-Console.Write("Введите b1: ");
-double b1 = double.Parse(Console.ReadLine()!);
-Console.Write("Введите b2: ");
-double b2 = double.Parse(Console.ReadLine()!);
-Get_Dot(k1, k2, b1, b2);
+Console.Write("Введите кол-во строк массива: ");
+int rows = int.Parse(Console.ReadLine()!);
+Console.Write("Введите кол-во столбцов массива: ");
+int cols = int.Parse(Console.ReadLine()!);
+int[,] array = GetArray(rows, cols);
+PrintArray(array);
+Find_average(array);
